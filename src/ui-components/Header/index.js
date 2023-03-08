aadddbb334ba658c9c658ddd40beda39a8775800
imran1404 <imran7098khan@gmail.com>
2023-03-08 18:30:07 +0530

    dropdown and notification updates

--- src/ui-components/Header/index.js
+++ src/ui-components/Header/index.js
@@ -1,5 +1,7 @@
 import { useRef } from "react";
-import { IoNotificationsOutline } from "react-icons/io5";
+import { IoNotificationsOutline, IoExtensionPuzzleOutline, IoSettingsOutline } from "react-icons/io5";
+import { FiUser, FiLogOut, FiHelpCircle } from "react-icons/fi";
+import { AiOutlineFileText } from "react-icons/ai";
 import DropdownMenu from "../DropdownMenu";
 import IconWrapper from "../IconWrapper";
 import UserIcon from "../UserIcon";
@@ -22,7 +24,7 @@ Dropdown Menu props are
 
 */
 
-const NotificationsIcon = ({ onClick = () => {} }) => (
+const NotificationsIcon = ({ onClick = () => { } }) => (
   <IconWrapper
     onClick={onClick}
     style={{
@@ -41,16 +43,33 @@ const Header = () => {
       <div className={styles["right-items"]}>
         <ul className={styles["header-navigations"]}>
           <li>
-            <DropdownMenu CustomMenu={NotificationsIcon}>
-              <ul style={{}}>
-                <li>Notifications 2</li>
-                <li>Notifications 1</li>
-                <li>Notifications 3</li>
-                <li>Notifications 4</li>
+            <DropdownMenu  CustomMenu={NotificationsIcon}>
+              <div className={styles["dropdown"]}>
+              <div style={{boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'}}>
+              <div style={{display:'flex',padding: '20px 20px', justifyContent: 'space-between'}}><h3>Notifications</h3><p style={{color: 'blue', fontWeight: 'bold'}}>Mark all as Read</p></div>
+              <div >
+                <ul className={styles["notification-menu"]}>
+                  <li>All</li>
+                  <li>Following</li>
+                  <li>Archeive</li>
+                </ul>
+              </div>
+              </div>
+              <ul className={styles["notification-tab"]}>
+                <li><img src="https://cdn.pixabay.com/photo/2017/03/19/20/19/ball-2157465__340.png" alt="" /><div className={styles["single-notification"]}><p>Jacob jone mwntion you in rewrite button tab</p>
+                  <p>1:12pm</p></div></li>
+                <li><img src="https://cdn.pixabay.com/photo/2017/03/19/20/19/ball-2157465__340.png" alt="" /><div className={styles["single-notification"]}><p>Jacob jone mwntion you in rewrite button tab</p>
+                  <p>1:12pm</p></div></li>
+                <li><img src="https://cdn.pixabay.com/photo/2017/03/19/20/19/ball-2157465__340.png" alt="" /><div className={styles["single-notification"]}><p>Jacob jone mwntion you in rewrite button tab</p>
+                  <p>1:12pm</p></div></li>
+                <li><img src="https://cdn.pixabay.com/photo/2017/03/19/20/19/ball-2157465__340.png" alt="" /><div className={styles["single-notification"]}><p>Jacob jone mwntion you in rewrite button tab</p>
+                  <p>1:12pm</p></div></li>
+                
               </ul>
+              </div>
             </DropdownMenu>
           </li>
-          
+
 
           <li>
             {/* User Dropdown Menu */}
@@ -59,18 +78,21 @@ const Header = () => {
               CustomMenu={UserIcon}
               dropdownContainerStyle={
                 {
-                  //padding: '15px 0'
+                  // padding: '15px 0'
                 }
               }
             >
-              <ul style={{}}>
-                <li>Item 2</li>
-                <li>Item 1</li>
-                <li>Item 3</li>
-                <li>Item 4</li>
+              <ul className={styles["dropdown-menu"]} >
+                <li><FiUser /><span>Profile</span></li>
+                <li><IoExtensionPuzzleOutline /><span>Integrations</span></li>
+                <li><IoSettingsOutline /><span>Settings</span></li>
+                <li><AiOutlineFileText /><span>Guide</span></li>
+                <li><FiHelpCircle /><span>Help Center</span></li>
+                <li><FiLogOut /><span>Logout</span></li>
               </ul>
             </DropdownMenu>
           </li>
+
         </ul>
       </div>
     </section>
