b089d1b9c6d9fe30376e6f00b99837d617c565d4
Rizwan <rizwankhan.august16@gmail.com>
2023-05-09 23:12:57 +0530

    ignore strict mode

--- pages/responsive.js
+++
@@ -1,8 +0,0 @@
-import Responsive from "../src/Pages/responsive";
-import Layout from "../src/ui-components/layout";
-
-export default () => (
-    <Layout>
-        <Responsive />
-    </Layout>
-);
